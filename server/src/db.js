534c8623b217595d1809b881e2ed24d3f6da3be9
Frhan K <farhank2613@gmail.com>
2026-08-02 21:46:50 +0500

    debug: log DATABASE_URL status on startup

--- server/src/db.js
+++ server/src/db.js
@@ -1,6 +1,9 @@
 import pg from 'pg';
 const { Pool } = pg;
 
+console.log('DATABASE_URL defined:', !!process.env.DATABASE_URL);
+console.log('DATABASE_URL prefix:', process.env.DATABASE_URL ? process.env.DATABASE_URL.substring(0, 20) + '...' : 'MISSING');
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
